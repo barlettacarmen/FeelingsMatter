eec05518ffe40a1499b282afc833b4a584a3f89b
Carmen Barletta <barlettacarmen@gmail.com>
2017-12-06 21:50:12 +0100

    Almost complete web UI

--- SecondPrototype/firebase.js
+++ SecondPrototype/firebase.js
@@ -19,14 +19,15 @@ var interButton=document.getElementById("b4");
 var stressButton=document.getElementById("b6");
 var relaxButton=document.getElementById("b5");
 
-if(sessionStorage.emotiv){
-  sogliaMin=sessionStorage.getItem('min');
-  sogliaMax=sessionStorage.getItem('max');
+if(sessionStorage.getItem('emotiv')){
+  sogliaMin=Number(sessionStorage.getItem('min'));
+  sogliaMax=Number(sessionStorage.getItem('max'));
+//  window.alert(" "+sogliaMax+" "+sogliaMin);
 }else{
   sogliaMax=100;
   sogliaMin=0;
 }
-
+//window.alert(" "+sogliaMax+" "+sogliaMin+" "+sessionStorage.getItem('emotiv'));
 //What to do after "on" events
 function updateButtonValue(name,element,val, glow){
   element.value=name+" "+val+"%"; //da controllare se fa il concatenamento
@@ -34,35 +35,117 @@ function updateButtonValue(name,element,val, glow){
     element.style.animation=glow+' 1500ms infinite';
     setTimeout(function(){
       element.style.animation='none';
-    },4000);
+    },5000);
     }
 
 }
+//Alerts
+function tempAlert(msg,duration)
+{
+     var el = document.createElement("div");
+     el.setAttribute("style","position:relative;width:200px;top:40%;left:85%;margin:3px;background-color:#ff6600;font-family: Arial;font-size: 16px;color: #FFFFFF;");
+     el.innerHTML = msg;
+     setTimeout(function(){
+      el.parentNode.removeChild(el);
+     },duration);
+     document.body.appendChild(el);
+}
 
-
-//Do the update
+//Do the update and trigger alerts
 var allEmotions=database.ref('emotions');
+/*allEmotions.on('value', function(snapshot){
+  window.alert(snapshot.val().stress);
+});*/
 var engagementRef=database.ref('emotions/engagement');
 engagementRef.on('value', function(snapshot){
   updateButtonValue("Engagement",engButton,snapshot.val(), 'glowing1');
+  if(snapshot.val()>=sogliaMax || snapshot.val()<=sogliaMin)
+    tempAlert("Info! Engagement value: "+snapshot.val()+"% outside the normal range",5000);
 });
 var excitementRef=database.ref('emotions/excitement');
 excitementRef.on('value', function(snapshot){
   updateButtonValue("Excitement",excitButton,snapshot.val(), 'glowing2');
+  if(snapshot.val()>=sogliaMax || snapshot.val()<=sogliaMin)
+    tempAlert("Info! Excitement value: "+snapshot.val()+"% outside the normal range",5000);
 });
 var focusRef=database.ref('emotions/focus');
 focusRef.on('value', function(snapshot){
   updateButtonValue("Focus",focusButton,snapshot.val(),'glowing3');
+  if(snapshot.val()>=sogliaMax || snapshot.val()<=sogliaMin)
+    tempAlert("Info! Focus value: "+snapshot.val()+"% outside the normal range",5000);
 });
 var interestRef=database.ref('emotions/interest');
 interestRef.on('value', function(snapshot){
   updateButtonValue("Interest",interButton,snapshot.val(),'glowing4');
+  if(snapshot.val()>=sogliaMax || snapshot.val()<=sogliaMin)
+    tempAlert("Info! Interest value: "+snapshot.val()+"% outside the normal range",5000);
 });
 var relaxRef=database.ref('emotions/relax');
 relaxRef.on('value', function(snapshot){
   updateButtonValue("Relaxation",relaxButton,snapshot.val(),'glowing6');
+  if(snapshot.val()>=sogliaMax || snapshot.val()<=sogliaMin)
+    tempAlert("Info! Relax value: "+snapshot.val()+"% outside the normal range",5000);
 });
 var stressRef=database.ref('emotions/stress');
 stressRef.on('value', function(snapshot){
   updateButtonValue("Stress",stressButton,snapshot.val(),'glowing5');
+  if(snapshot.val()>=sogliaMax || snapshot.val()<=sogliaMin)
+    tempAlert("Info! Stress value: "+snapshot.val()+"% outside the normal range",5000);
 });
+
+var obj = {
+ emotions:[
+ {name:"excitement", values:[]},
+ {name:"realx", values: []},
+ {name:"stress", values:[]},
+ {name:"engagement", values: []},
+ {name:"interest", values:[]},
+ {name:"focus", values: []}
+]
+};
+if(localStorage.getItem('experience')!=null){
+    let emo=JSON.parse(localStorage.getItem('experience'));
+    for(let k=0;k<6;k++)
+    obj.emotions[k].values=obj.emotions[k].values.concat(emo.emotions[k].values);}
+
+allEmotions.on('value', function(snapshot){
+  obj.emotions[0].values.push(snapshot.val().excitement);
+  obj.emotions[1].values.push(snapshot.val().relax);
+  obj.emotions[2].values.push(snapshot.val().stress);
+  obj.emotions[3].values.push(snapshot.val().engagement);
+  obj.emotions[4].values.push(snapshot.val().interest);
+  obj.emotions[5].values.push(snapshot.val().focus);
+
+
+  localStorage.setItem('experience', JSON.stringify(obj));
+
+  //window.alert(""+localStorage.getItem('experience'));
+  //localStorage.clear();
+});
+
+
+
+//Video
+
+  function BeginListening(){
+   //After the first load, we can begin listening for new values.
+   setTimeout(function(){
+   var play = database.ref('/Play');
+   var videoId = database.ref('/videoID/ID');
+   videoId.on('value', function(snapshot){
+     gameInstance.SendMessage('360VideoPlayer','PlayYoutubeVideo', snapshot.val());
+   });
+
+
+   play.on('value', function(snapshot){
+     gameInstance.SendMessage('360VideoPlayer', 'PlayButtonFirebase', snapshot.val());
+   });
+
+   var cameraRotation = database.ref('cameraRotation');
+   cameraRotation.on('value', function(snapshot){
+     gameInstance.SendMessage('Main Camera', 'setTempX', snapshot.child('X').val());
+     gameInstance.SendMessage('Main Camera', 'setTempY', snapshot.child('Y').val());
+     gameInstance.SendMessage('Main Camera', 'UpdateFromFirebase');
+   });
+ }, 5000);
+ }
---
+++ SecondPrototype/report.js
@@ -0,0 +1,86 @@
+var obj = {
+ emotions:[
+ {name:"excitement", values:[1,2,3,4,5,6,7,8,9,10,11,12,12,13,14,15,16,17,18,19,20,21,22]},
+ {name:"realx", values: [67,1,2,3,5,7,4,8,99,44,22,45,11,10,90,40,66,33,55,89,10,30,23,5]},
+ {name:"stress", values:[10,50,88,70,12,59,70,66,88,1,22,44,79,50,2,1,3,4,5,6,78,8,9]},
+ {name:"engagement", values: [90,50,88,70,12,59,70,66,88,99,22,44,79,50,2,44,3,4,5,6,78,8,9]},
+ {name:"interest", values:[90,50,61,70,12,59,70,66,88,99,22,44,79,50,2,1,60,4,5,6,78,8,9]},
+ {name:"focus", values: [55,50,88,70,12,59,70,66,88,100,22,44,79,50,2,1,3,4,5,6,1,2,3]}
+]
+};
+localStorage.setItem('experience', JSON.stringify(obj));
+let emoExp=JSON.parse(localStorage.getItem('experience'));
+let excData=emoExp.emotions[0].values;
+let relData=emoExp.emotions[1].values;
+let strData=emoExp.emotions[2].values;
+let engData=emoExp.emotions[3].values;
+let intData=emoExp.emotions[4].values;
+let focData=emoExp.emotions[5].values;
+let eng={label:"Engagement",
+  fillColor: "rgba(220,220,220,0)",
+  strokeColor: "rgba(117,197, 199,1)",
+  pointColor: "rgba(117,197, 199,1)",
+  data:engData}
+
+let excit={label:"Excitement",
+  fillColor: "rgba(220,220,220,0)",
+  strokeColor: "rgba(224, 203, 78,1)",
+  pointColor: "rgba(224, 203, 78,1)",
+  data:excData}
+
+let foc={label:"Focus",
+    fillColor: "rgba(220,220,220,0)",
+    strokeColor: "rgba(141,224, 130,1)",
+    pointColor: "rgba(141,224, 130,1)",
+    data:focData}
+let inter={label:"Interest",
+    fillColor: "rgba(220,220,220,0)",
+    strokeColor: "rgba(220,150, 83,1)",
+    pointColor: "rgba(220,150, 83,1)",
+    data:intData}
+
+let rel={label:"Relaxation",
+  fillColor: "rgba(220,220,220,0)",
+  strokeColor: "rgba(122,175, 235,1)",
+  pointColor: "rgba(122,175, 235,1)",
+  data:relData}
+let str={
+  label:"Stress",
+  fillColor: "rgba(220,220,220,0)",
+  strokeColor: "rgba(156,128,239,1)",
+  pointColor: "rgba(156,128,239,1)",
+  data:strData}
+let data =[excit,rel,str,eng,inter,foc];
+let canva=[];
+for(let k=0;k<7;k++)
+    canva[k]=document.getElementById('canvas'+k).getContext("2d");
+let lc=[];
+for(let k=0;k<6;k++)
+    lc[k]=new Chart(canva[k]).Line({labels:[],datasets: [data[k]]},
+      {
+      datasetStrokeWidth:3,
+      scaleOverride : true,
+      scaleSteps : 10,
+      scaleStepWidth : 10,
+      scaleStartValue : 0,
+      pointDotRadius: 0,
+      bezierCurve: true,
+      scaleShowVerticalLines: false,
+      scaleGridLineColor: "rgba(0,0,0,0.2)",
+      responsive: true,
+      barValueSpacing: 2
+    });
+lc[6]=new Chart(canva[6]).Line({labels:[],datasets: data},
+  {
+  datasetStrokeWidth:3,
+  scaleOverride : true,
+  scaleSteps : 10,
+  scaleStepWidth : 10,
+  scaleStartValue : 0,
+  pointDotRadius: 0,
+  bezierCurve: true,
+  scaleShowVerticalLines: false,
+  scaleGridLineColor: "rgba(0,0,0,0.2)",
+  responsive: true,
+  barValueSpacing: 2
+});
